758776cbd3a2f0d6c694a3de15e6333a622edcb5
jeffreyfox <jeffreyfox@users.noreply.github.com>
2015-03-03 22:03:03 -0800

    Update test_array.cpp

--- test_array.cpp
+++ test_array.cpp
@@ -8,4 +8,6 @@ void testArray(int argc, char* argv[])
 	int b[8] = {-2, -3, -4, -1, -2, -1, -5, -3};
 	cout << "max-subarray (allowing empty) is " << maxSubArray(b, 8) << endl;
 	cout << "max-subarray (not allowing empty) is " << maxSubArray2(b, 8) << endl;
+	int c[5] = {3, 2, 5, 10, 7};
+	cout << "max-subarray (non-consecutive) is " << maxSubArray3(c, sizeof(c)/sizeof(int)) << endl;
 }
