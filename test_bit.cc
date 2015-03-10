744e3589e63acc38f05d2f4ab6ffc288c0399fea
jeffreyfox <jeffreyfox@users.noreply.github.com>
2015-03-10 09:06:57 -0700

    Update test_bit.cc

--- test_bit.cc
+++ test_bit.cc
@@ -4,31 +4,40 @@ void testBit(int argc, char* argv[])
 {
 	unsigned x = 23;
 	unsigned y = swapBits(x);
-	cout << " " << bin2str(x) << " " << x << " swapped as " <<endl;
-	cout << " " << bin2str(y) << " " << y << endl;
-	x = 20387;
-	unsigned k = 6;
-	y = turnOffK(x, k);
-	cout << " " << bin2str(x) << " " << x << " turn off bit " << k << " becomes " <<endl;
-	cout << " " << bin2str(y) << " " << y << endl;
-	x = 1;
-	if(isPowerOfTwo(x)) cout << " " << bin2str(x) << " " << x << " is power of two " << endl;
-	else cout << " " << bin2str(x) << " " << x << " is NOT power of two " << endl;
-	int z = 1324987; 
-	cout << " " << bin2str(z) << " " << z << " has " << numberOfOne(z) << " 1s" <<endl;
-	cout << " " << bin2str(z) << " " << z << " has " << numberOfOne2(z) << " 1s" <<endl;
+	cout << " " << dec2bin(x) << " " << x << " swapped as " <<endl;
+	cout << " " << dec2bin(y) << " " << y << endl;
+	
+	x = 16;
+	if(isPowerOfTwo(x)) cout << " " << dec2bin(x) << " " << x << " is power of two " << endl;
+	else cout << " " << dec2bin(x) << " " << x << " is NOT power of two " << endl;
 
-	x = 128+64+32+1024;
-	y = findHigher(x);
-	cout << "x = " << bin2str(x) << " " << x << endl;
-	cout << "y = " << bin2str(y) << " " << y << endl;
-	z = x&(~x+1);
-	cout << "z = " << bin2str(z) << " " << z << endl;
+	int z = 1324987; 
+	cout << " " << dec2bin(z) << " " << z << " has " << numSetBits(z) << " 1s" <<endl;
+	cout << " " << dec2bin(z) << " " << z << " has " << numSetBits2(z) << " 1s" <<endl;
 
 	int i = 2, j = 5;
 	unsigned p = setBits(i, j);
-	cout << "p = " << bin2str(p) << " " << p << endl;
+	cout << "p = " << dec2bin(p) << " " << p << endl;
 	unsigned kk = 0;
 	p = setBits2(i, j);
-	cout << "p = " << bin2str(p) << " " << p << endl;
+	cout << "p = " << dec2bin(p) << " " << p << endl;
+
+	i = 2, j = 31;
+	int n = 1024, m = ~0;
+	cout << " i = " << i << " j = " << j << endl;
+	cout << "m = " << dec2bin(m) << " " << m << endl;
+	cout << "n = " << dec2bin(n) << " " << n << endl;
+	int q = updateBits(n, m, i, j);
+	cout << "q = " << dec2bin(q) << " " << q << endl;
+	q = updateBits2(n, m, i, j);
+	cout << "q = " << dec2bin(q) << " " << q << endl;
+
+	m = 127;
+	n = nextHigher(m);
+	p = nextHigher2(m);
+	q = nextLower(m);
+	cout << "m = " << dec2bin(m) << " " << m << endl;
+	cout << "n = " << dec2bin(n) << " " << n << endl;
+	cout << "p = " << dec2bin(p) << " " << p << endl;
+	cout << "q = " << dec2bin(q) << " " << q << endl;
 }
