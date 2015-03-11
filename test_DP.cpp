437c578063d8bf86efba1751a126d8fcdadc8771
jeffreyfox <jeffreyfox@users.noreply.github.com>
2015-03-11 09:18:11 -0700

    Update test_DP.cpp

--- test_DP.cpp
+++ test_DP.cpp
@@ -4,8 +4,15 @@ void testDP(int argc, char* argv[])
 {
 	//string x = "abcbdab", y = "bdcaba";
 	string x = "10010101", y = "010110110";
-	LCS lcs(x, y);
-	int l = lcs.LCSLength();
+	LCS lcs(x, y), lcs2(x, y), lcs3(x, y);
+	int l = lcs.LCSLength(), l2 = lcs2.LCSLength2(), l3 = lcs3.LCSLength3();
 	cout << "LCS of <" << x << "> and <" << y << "> is <" << lcs.constructLCS() << ">, length = " << l << endl;
+	cout << "LCS of <" << x << "> and <" << y << " > has length = " << l2 << endl;
+	cout << "LCS of <" << x << "> and <" << y << " > has length = " << l3 << endl;
 	lcs.print2D();
+	cout << endl;
+	lcs2.print2D();
+	cout << endl;
+	lcs3.print2D();
+	cout << endl;
 }
