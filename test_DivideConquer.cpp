2d7ab8fa59e343b360ceb3b9dfca804bc2543269
jeffreyfox <jeffreyfox@users.noreply.github.com>
2015-03-26 09:27:40 -0700

    Update test_DivideConquer.cpp

--- test_DivideConquer.cpp
+++ test_DivideConquer.cpp
@@ -30,4 +30,21 @@ void testDivideConquer(int argc, char* argv[])
 	test4.solve1(); test4.printSol();
 	test5.solve2(); test5.printSol();
 	test6.solve3(); test6.printSol();
+	
+	//nearest points:
+	int seed = 129;
+	int N = 300, m = N;
+	vector<Point> pts(N);
+	for(int i = 0; i < N; ++i) {
+		pts[i].x = m*1.0*rand()/RAND_MAX;
+		pts[i].y = m*1.0*rand()/RAND_MAX;
+		//	cout << "(" << pts[i].x << " " << pts[i].y << ")" << endl;
+	}
+
+	NearestPoints np(pts);
+
+	double md1 = np.solve1();
+	cout << "minimum distance d1 = " << md1 << endl;
+	double md2 = np.solve2();
+	cout << "minimum distance d2 = " << md2 << endl;
 }
