1ad8f8f20a06a3e591d4ab2a0ced5c4d30fdea78
jeffreyfox <jeffreyfox@users.noreply.github.com>
2015-04-11 12:48:35 -0700

    Update test_tree.cpp

--- test_tree.cpp
+++ test_tree.cpp
@@ -6,11 +6,12 @@ extern ostream& operator<<(ostream& os, const vector<int>& vec);
 
 void testBST(int argc, char* argv[])
 {
-	int i = 0, N = 13, m = 50, seed = 10;
+	int i = 0, N = 30, m = 100, seed = 800;
 	BSTree bst;
 
 	if(seed != 0) { //initialize randomly
 		srand(seed);
+		for(int i = 0; i < 1000000; ++i) rand();
 		for(i = 0; i < N; ++i) { 
 			int key = m*(1.0*rand()/RAND_MAX); 
 			if(bst.search(key) == bst.nullnode()) {
@@ -35,8 +36,8 @@ void testBST(int argc, char* argv[])
 	bst.printLevelOrder();
 	cout << endl;
 
-	cout << "Searching keys upto " << m << " ... " << endl;
-	for(int k = 0; k <= m; k+=1) {
+	cout << "Removing keys between " << m/4 << " and " << m*3/4 << " ... " << endl;
+	for(int k = m/4; k <= m*3/4; k++) {
 		BSTreeNode *t = bst.search(k);
 		if(t != bst.nullnode()) { 
 			cout << "("<<t->key << ") ";
@@ -46,11 +47,12 @@ void testBST(int argc, char* argv[])
 	cout << endl;
 	cout << "Binary search tree in-order : " << endl;
 	bst.printInOrder(); cout << endl;
+	cout << endl;
 }
 
 void testRBT(int argc, char* argv[])
 {
-	int i = 0, N = 13, m = 50, seed = 10;
+	int i = 0, N = 30, m = 50, seed = 10;
 	RBTree rbt;
 
 	if(seed != 0) { //initialize randomly
@@ -79,15 +81,18 @@ void testRBT(int argc, char* argv[])
 	rbt.printLevelOrder();
 	cout << endl;
 
-	cout << "Searching keys upto " << m << " ... " << endl;
-	for(int k = 0; k <= m; k+=1) {
+	cout << "Removing keys between " << m/4 << " and " << m*3/4 << " ... " << endl;
+	for(int k = m/4; k <= m*3/4; k++) {
 		RBTreeNode *t = rbt.search(k);
 		if(t != rbt.nullnode()) { 
 			cout << "("<<t->key << ") ";
-			rbt.remove(t);
+			rbt.remove2(t);
 		}
 	}
 	cout << endl;
 	cout << "Red-black tree in-order : " << endl;
 	rbt.printInOrder(); cout << endl;
+	cout << "Red-black tree level-order : " << endl;
+	rbt.printLevelOrder();
+	cout << endl;
 }
