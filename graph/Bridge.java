0eb2f5cb5e355c4bdc7cc13d85a2d7d569ace81e
jeffreyfox <jeffreyfox.chen@gmail.com>
2015-05-30 13:54:14 -0700

    add Bridge.java and txt files

--- graph/Bridge.java
+++ graph/Bridge.java
@@ -1,24 +1,21 @@
 import java.util.LinkedList;
 
-/// class to find all articulation points of a graph
-/// Criteria for AP:
-/// 1. vertex v is root of dfs tree and has more than 1 child. 
-/// 2. vertex v is non root, not leaf, and has at least one subtree rooted at 
-/// a child, and that subtree does NOT have a back edge that climbs ¡°higher¡± 
-/// than v in the dfs tree.
+/// class to find all bridges of a graph
+/// Criteria for bridge: 
+/// tree edge (v, w), where v's child is w, and subtree w does NOT 
+/// have a back edge that climbs to or higher than v in the dfs tree.
 
-public class ArticulationPoint {
+public class Bridge {
 
     public static void main(String[] args) {
         String[] files = {"g1", "g2", "g3", "g4"};
         for (String s : files) {
             In in = new In(s + ".txt");
             Graph g = new Graph(in);            
-            ArticulationPoint apfinder = new ArticulationPoint(g);
-            if(apfinder.hasAP()) {
-                StdOut.print("Graph " + s + " has articulation points: ");
-                for (int v : apfinder.APs()) 
-                    StdOut.print(v + " ");
+            Bridge bridgefinder = new Bridge(g);
+            if(bridgefinder.hasBrdige()) {
+                StdOut.print("Graph " + s + " has bridges points: ");
+                bridgefinder.printBridges();                
                 StdOut.println();
             } else {
                 StdOut.print("Graph " + s + " has NO articulation points\n");
@@ -27,23 +24,30 @@ public class ArticulationPoint {
     }
 
     private boolean[] marked; //whether vertex is visited?
-    private boolean[] isAP; //whether vertex is articulation point?
     private int[] parent; //vertex's parent in dfs search tree
     private int[] disc; //discover times of the vertex
     private int[] low; //minimum discover times of vertices reachable from subtree rooted at v 
     private int time = 0; //time stamp of dfs
-    private boolean hasAP = false; //whether graph has any APs?
+    private LinkedList<Edge> bridges;
 
+    private class Edge {
+        private int v, w;
+        Edge(int v, int w) {
+            this.v = v;
+            this.w = w;
+        }
+    }
     // find articulation points of an undirected graph
-    public ArticulationPoint(Graph G) {
+    public Bridge(Graph G) {
         marked = new boolean[G.V()];
-        isAP = new boolean[G.V()];
         parent = new int[G.V()];
         disc = new int[G.V()];
         low = new int[G.V()];        
         for (int v = 0; v < G.V(); ++v) 
             parent[v] = -1;
-
+        
+        bridges = new LinkedList<Edge>();
+        
         for (int v = 0; v < G.V(); ++v) {
             if (!marked[v]) dfs(G, v);
         }
@@ -53,32 +57,24 @@ public class ArticulationPoint {
     private void dfs(Graph G, int v) {
         marked[v] = true;
         disc[v] = ++time;
-        low[v] = disc[v];
-        int children = 0;
+        low[v] = disc[v];        
         for (int w : G.adj(v)) //search thru v's neighbor w
             if (!marked[w]) { //w unvisited, and becomes the descendant of v in dfs tree
-                children ++;
                 parent[w] = v;
                 dfs(G, w);               
                 low[v] = Math.min(low[v], low[w]); //after finishing w, update v's low value from w
-                // check if v is AP
-                if (parent[v] == -1 && children > 1) isAP[v] = true;
-                if (parent[v] != -1 && low[w] >= disc[v]) isAP[v] = true;
-                if(isAP[v]) hasAP = true;
+                // check if v-w is bridge
+                if (low[w] > disc[v]) bridges.add(new Edge(v, w));               
             } else if (parent[v] != w) { //w visited (back edge), check if w is not v's direct parent
                 low[v] = Math.min(low[v], disc[w]); //update v's low value
             }
     }
     // has articulation points?
-    public boolean hasAP() { return hasAP; }
+    public boolean hasBrdige() { return !bridges.isEmpty(); }
 
     // all articulation points, null if none
-    Iterable<Integer> APs() {
-        if (!hasAP()) return null;
-        LinkedList<Integer> ap = new LinkedList<Integer>();        
-        for (int v = 0; v < isAP.length; ++v)
-            if (isAP[v]) ap.add(v);            
-
-        return ap;
+    public void printBridges() {
+        for (Edge e : bridges)
+            StdOut.print(e.v + "-" + e.w + " ");       
     }
 }
