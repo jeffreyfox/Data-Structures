651332cf2f0259514ba0b2a9a3334d6977499483
jeffreyfox <jeffreyfox@users.noreply.github.com>
2015-06-01 19:50:31 -0800

    Update SimplifyMultiGraph.java

--- graph/SimplifyMultiGraph.java
+++ graph/SimplifyMultiGraph.java
@@ -1,8 +1,10 @@
-import java.util.LinkedList;
-
 // Simplify multigraph (directed) and remove duplicated edges and self loops
 // Final graph is conceptually undirected, but use a digraph to represent for 
 // implementation convenience
+// The resulting graph's adjacency list is constructed in the way
+// that the added vertices' indices are in ascending order (e.g.
+// for vertex 3, 2nd iteration adds vertex 0, 2 to its adjacency list,
+// 3rd iteration adds vertex 4, 6 to its adjacency list
 public class SimplifyMultiGraph {
 	public static void main(String[] args) {
 		int V = 5;
@@ -29,7 +31,8 @@ public class SimplifyMultiGraph {
 				if (j != i) G.addEdge(j, i);
 			}
 		}
-		//second iteration, add neighbors in ascending order
+
+		//second iteration, add neighbors of j that is smaller than j
 		for (int i = 0; i < G.V(); ++i) {
 			for (int j : G.adj(i)) {
 				if (j > i) 
@@ -38,7 +41,7 @@ public class SimplifyMultiGraph {
 						GS.addEdge(j, i);
 			}
 		}
-		//third iteration, add neighbors in descending order
+		//third iteration, add neighbors of j that is larger than j
 		for (int i = 0; i < G.V(); ++i) {
 			for (int j : G.adj(i)) {
 				if (j < i) 
