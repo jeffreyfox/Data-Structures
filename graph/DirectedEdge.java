e85f37ef7bd0c56f3f0d882bb1bbd604d3b9037e
jeffreyfox <jeffreyfox@users.noreply.github.com>
2015-06-06 09:11:59 -0800

    Create DirectedEdge.java

---
+++ graph/DirectedEdge.java
@@ -0,0 +1,28 @@
+
+// class for a directed edge
+public class DirectedEdge implements Comparable<DirectedEdge> {
+	private final int v;
+	private final int w;
+	private final double weight;
+
+	public DirectedEdge(int v, int w, double weight) {
+		this.v = v;
+		this.w = w;
+		this.weight = weight;
+	}
+
+	public int from() { return v; }
+	public int to() { return w; }
+
+	public double weight() { return weight; }
+
+	public int compareTo(DirectedEdge that) {
+		if      (this.weight < that.weight) return -1;
+		else if (this.weight > that.weight) return  1;
+		else                                return  0;
+	}
+
+	public String toString() {
+		return String.format("%d->%d %4.2f", v, w, weight);
+	}
+}
