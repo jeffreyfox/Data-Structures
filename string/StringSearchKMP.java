0638939287ed35a81361fbc267bd078217df2b4b
jeffreyfox <jeffreyfox.chen@gmail.com>
2015-07-12 10:47:36 -0700

    update StringSearchKMP.java

--- string/StringSearchKMP.java
+++ string/StringSearchKMP.java
@@ -1,6 +1,10 @@
 /// KMP pattern search algorithm
+/// The idea is to analyze the pattern first, and prestore the jump position of j after matching txt with pat[j]
+/// Method 1 finds the position by finding incrementally the jump position of matching pat[1..j] and store it.
+/// Method 2 finds the position by finding iteratively the longest prefix of pat[0..j-1] that is also a suffix.
+
 public class StringSearchKMP {
-    // using dfa[][] array and X[]
+    // method 1: using dfa[][] array and X[]
     public static int search(String txt, String pat) {
         int n = txt.length(), m = pat.length();
         int i, j;
@@ -39,7 +43,7 @@ public class StringSearchKMP {
         if (j == m) return i-m; //found
         else        return   n; //not found
     }
-    // simplified version, since we only need X of the previous step, just use one variable X instead of an array, and update X accordingly
+    // method 1 simplified version: we only need X of the last step, so just use one variable X instead of an array, and update X accordingly
     public static int search2(String txt, String pat) {
         int n = txt.length(), m = pat.length();
         int i, j;
@@ -66,12 +70,42 @@ public class StringSearchKMP {
         else        return   n; //not found
     }
 
+    // method 2: find length of longest (non-trivial) prefix of pat[0..j-1] (length j) that is also a suffix.
+    public static int search3(String txt, String pat) {
+        int n = txt.length(), m = pat.length();
+        int i, j;
+        int[] f = new int[m+1]; //f[j]: length of longest (non-trivial) prefix of pat[0..j-1] (length j) that is also a suffix.
+
+        //construct f iteratively
+        f[0] = -1; //define as -1 for later convenience
+        for (j = 1; j <= m; ++j) {
+            int k = f[j-1];
+            while (k >= 0 && pat.charAt(k) != pat.charAt(j-1)) k = f[k];
+            f[j] = k+1; //either k == -1 or pat[k] is end of the longest prefix
+        }
+
+        for (j = 0; j <= m; ++j) System.out.print("f[" + j + "]=" + f[j] + " ");
+        System.out.println();
+
+        //start matching using f to jump in pat
+        for (i = 0, j = 0; i < n && j < m; ) {
+            if (txt.charAt(i) == pat.charAt(j)) { i++; j++; } //matched
+            else if (j == 0)  i++; //match pat[0] with next char in txt
+            else j = f[j]; //i stay still, j moves to the char immediately after the longest prefix of pat[0..j-1]
+        }
+
+        if (j == m) return i-m; //found
+        else        return   n; //not found
+    }
+
     public static void main(String[] args) {
         String txt = args[0];
         String pat = args[1];
         int offset = StringSearchKMP.search(txt, pat);
         int offset2 = StringSearchKMP.search2(txt, pat);
+        int offset3 = StringSearchKMP.search3(txt, pat);
         if (offset != offset2) System.out.println("Warning: offset2 and offset are not equal!");
+        if (offset != offset3) System.out.println("Warning: offset3 and offset are not equal!");
         if (offset == txt.length()) System.out.println("Pattern \"" + pat + "\" not found in \"" + txt + "\"!");
         else {
             System.out.println("Pattern \"" + pat + "\" found in \"" + txt + "\".");
