01b8428037f76f69fc0e81f3b5d48b08a3e44aa6
jeffreyfox <jeffreyfox.chen@gmail.com>
2015-08-16 12:46:57 -0700

    move mysql to bitbucket repos

--- mysql/query.cmd
+++
@@ -1,7 +0,0 @@
- select County, District, Count(*) as cnt from CombinedRef where API_as_avg_nw > 900 group by District ORDER BY cnt DESC;
-
-SELECT County, District, School, City, Zip, API_as_avg_nw, ETOT_asian, ETOT_all, asian_pct
-FROM `CombinedRef`
-WHERE API_as_avg_nw >900
-AND ETOT_asian >500
-ORDER BY `CombinedRef`.`ETOT_asian` DESC 
